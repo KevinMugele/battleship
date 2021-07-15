f3f9a8f5ce58ad96332c6dee90cf205bd4fac9a8
Kevin Mugele <kevin.mugele@gmail.com>
2021-07-15 15:37:25 -0400

    Add spec to make sure ships are not able to overlap

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -93,4 +93,13 @@ RSpec.describe Board do
     expect(cell_3.ship).to eq(cruiser)
     expect(cell_3.ship).to eq(cell_2.ship)
   end
+
+  it "checks if ships are overlapping" do
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    board.place(cruiser, ["A1", "A2", "A3"])
+    submarine = Ship.new("Submarine", 2)
+
+    expect(board.valid_placement?(submarine, ["A1", "B1"])).to eq(false)
+  end
 end
