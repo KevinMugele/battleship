fce0d7ea6540d5de940fe845f5fb87aeacde8d85
Kevin Mugele <kevin.mugele@gmail.com>
2021-07-14 20:19:13 -0400

    Add multiple methods to check valid placement, still need to get diagonal to function properly

--- lib/board.rb
+++ lib/board.rb
@@ -27,4 +27,31 @@ class Board
       @cells[coordinate].empty?
     end
   end
+
+  def consecutive?(coordinates)
+    vertical_column(coordinates) || horizontal_row(coordinates)
+  end
+
+  def vertical_column(coordinates)
+    coordinates.all? do |coordinate|
+      coordinate[0] == coordinates.first[0]
+    end
+  end
+
+  def vertical_consecutive(coordinates)
+    ship_length = coordinates.length
+    (1..4).each_cons(ship_length) do |x|
+      #make array of results, check results to coordinates, coordinates need change -
+  end
+
+  def horizontal_row(coordinates)
+    coordinates.all? do |coordinate|
+      coordinate[1] == coordinates.first[1]
+    end
+  end
+
+  def valid_placement?(ship, coordinates)
+    ship.length == coordinates.size && not_occupied?(coordinates) && consecutive?(coordinates)
+  end
+
 end
