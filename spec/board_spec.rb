95c5edf2721a9613bced0972dfd94a3e0787d09d
Kevin Mugele <kevin.mugele@gmail.com>
2021-07-15 15:34:22 -0400

    Add place method and tests

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -35,13 +35,38 @@ RSpec.describe Board do
 
     expect(board.valid_placement?(cruiser, ["A1", "A2"])).to eq(false)
     expect(board.valid_placement?(submarine, ["A2", "A3", "A4"])).to eq(false)
-    expect(board.valid_placement?(submarine, ["A1", "C1"])).to eq(false)
+  end
+
+  it "can validate placements are consecutive" do
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2)
+    board.vertical_consecutive(["A1", "C1"])
+
+
+
     expect(board.valid_placement?(cruiser, ["A1", "A2", "A4"])).to eq(false)
     expect(board.valid_placement?(submarine, ["A1", "C1"])).to eq(false)
     expect(board.valid_placement?(cruiser, ["A3", "A2", "A1"])).to eq(false)
     expect(board.valid_placement?(submarine, ["C1", "B1"])).to eq(false)
-    expect(board.valid_placement?(cruiser, ["A1", "B2", "C3"])).to eq(false)
-    expect(board.valid_placement?(submarine, ["C2", "D3"])).to eq(false)
+    end
+
+  it "can validate placements are not diagonal" do
+      board = Board.new
+      cruiser = Ship.new("Cruiser", 3)
+      submarine = Ship.new("Submarine", 2)
+      board.vertical_consecutive(["A1", "C1"])
+
+
+      expect(board.valid_placement?(cruiser, ["A1", "B2", "C3"])).to eq(false)
+      expect(board.valid_placement?(submarine, ["C2", "D3"])).to eq(false)
+  end
+
+  it "can validate placements" do
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2)
+    board.vertical_consecutive(["A1", "C1"])
 
     expect(board.valid_placement?(submarine, ["A1", "A2"])).to eq(true)
     expect(board.valid_placement?(cruiser, ["B1", "C1", "D1"])).to eq(true)
@@ -53,5 +78,18 @@ RSpec.describe Board do
     submarine = Ship.new("Submarine", 2)
 
     expect(board.not_occupied?(["A1"])).to be true
+  end
+
+  it "places a ship" do
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    board.place(cruiser, ["A1", "A2", "A3"])
+    cell_1 = board.cells["A1"]
+    cell_2 = board.cells["A2"]
+    cell_3 = board.cells["A3"]
+
+    expect(cell_1.ship).to eq(cruiser)
+    expect(cell_2.ship).to eq(cruiser)
+    expect(cell_3.ship).to eq(cruiser)
   end
 end
