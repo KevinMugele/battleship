e99da5552fa0d5f800f610fd437b3f811eee78ea
Kevin Mugele <kevin.mugele@gmail.com>
2021-07-17 09:25:29 -0400

    Work on adding methods to get game to run. Add game_setup and work on setting boards up

--- lib/game.rb
+++ lib/game.rb
@@ -4,42 +4,60 @@ require './lib/ship'
 
 
 class Game
-  def initialize
 
+  attr_reader :player_board, :player_ships, :computer_board, :computer_ships
+
+  def initialize
+    @player_board = nil
+    @computer_board = nil
+    @player_ships = []
+    @computer_ships = []
   end
 
   def start
     main_menu
   end
 
+  def user_input
+    gets.chomp
+  end
+
   def main_menu
     puts "WELCOME TO BATTLESHIP!"
     puts "Enter (p) to play; (q) to quit."
-    player_input = gets.chomp.downcase
-    if player_input == "p"
-      play_game
-    elsif
-      player_input == "q"
-      exit_game
-    else
-      puts "Enter (p) to play; (q) to quit."
+
+    player_input = user_input.downcase
+
+    while player_input != "p" || player_input != "q"
+      if player_input == "p"
+        game_setup
+      elsif
+        player_input == "q"
+        exit_game
+      else
+        puts "Invalid input. Please enter (p) to play; (q) to quit."
+      end
     end
   end
 
-  def play_game
-    puts "I have laid out my ships on the grid."
+  def game_setup
+    computer_board_setup(board)
     puts "Place your ships on the board"
-    # need to make computer to place ships on board randomly.
-    #maybe .sample method works?
+    show_board_with_ships
+    puts "Enter the cells for the submarine(2 spaces)"
+    show_board_with_ships
+  end
+
+  def computer_board_setup(board)
+    #make board for computer
+    puts "I have laid out my ships on the grid."
+  end
+
+  def place_ships
     puts "The cruiser is 3 units long and the submarine is 2 units long."
     show_board(board)
     puts "Enter the cells of where you want to place the cruiser(3 spaces)"
-    #get user input for their spaces
-    show_board_with_ships
-    puts "Enter the cells for the submarine(2 spaces)"
-    #get user input for their spaces
-    #need to prompt user if invalid coordinates
-    show_board_with_ships
+
   end
 
   def show_board(board)
